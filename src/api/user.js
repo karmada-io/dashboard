3a9d2a6bc3a935ad35d08be742711e94f2d046e4
jhnine <jhnine@163.com>
2021-12-20 16:30:07 +0800

    initial version
    Signed-off-by: jhnine <jhnine@163.com>

---
+++ src/api/user.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+export function loginApi(token) {
+  return request({
+    url: '/apis/cluster.karmada.io/v1alpha1/clusters',
+    method: 'get',
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
+}
