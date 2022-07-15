b1f0d411be9be17a9b5a7bbd4114d9b996b234a4
Jun Xiang <jx2161@nyu.edu>
2022-07-14 22:59:21 -0400

    Add MUI Theme Variables
    Add fonts, platette colors, and etc.
    Source from figma design, https://www.figma.com/file/nAhWBN06v6hMCHDlIT9mzk/KARMADA?node-id=6%3A134.
    
    Also tweak eslint and prettier setup a bit to make it allow used variable and "auto" end of line. That  makes testing much easier.
    
    Signed-off-by: Jun Xiang <jx2161@nyu.edu>

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,6 +1,17 @@
 import { createTheme } from "@mui/material";
 
 const THEME = createTheme({
+  palette: {
+    primary: {
+      dark: "#30344E",
+      main: "#E9E9E9",
+      light: "#EFEFEF"
+    },
+    // Border
+    secondary: {
+      main: "#F5F5F5"
+    }
+  },
   breakpoints: {
     values: {
       xs: 0,
@@ -11,11 +22,22 @@ const THEME = createTheme({
     }
   },
   typography: {
-    fontFamily: '"Roboto Slab", serif'
+    fontFamily: '"Roboto Slab", serif',
+    h1: { fontSize: "24px" },
+    h2: { fontSize: "22px" },
+    h3: { fontSize: "16px" },
+    h4: { fontSize: "15px", fontWeight: "700" },
+    h5: { fontSize: "15px", fontWeight: "500" },
+    subtitle1: { fontSize: "14px" },
+    subtitle2: { fontSize: "10px" },
+    body1: { fontSize: "15px" },
+    body2: { fontSize: "13px" },
+    button: { fontSize: "14px" }
   },
   select: {
     fontFamily: '"Roboto Slab", serif'
-  }
+  },
+  drawerWidth: "240"
 });
 
 export default THEME;
