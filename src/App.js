a5de137602af1a3aefd206ba3a50d4f1d7cbe98f
Jun Xiang <jx2161@nyu.edu>
2022-08-07 23:53:16 -0400

    Rewrite Tests
    How to run:
    
    Pull request locally
    run "npm install"
    run "npm run test" to see jest unit test result.
    run "npm run start", navigate to "http://localhost:3000/playground" to see the demo.
    Note: MUI global theme changes are in PR#23. This PR doesn't contain global styling.
    
    Signed-off-by: Jun Xiang <jx2161@nyu.edu>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { useSelector } from "react-redux";
@@ -5,11 +6,9 @@ import LoginPage from "./pages/LoginPage";
 import Overview from "./pages/overview";
 import PageNotFound from "./pages/pagenotfound";
 import ProtectedRoute from "./routes/ProtectedRoutes";
-// test
-import Dashboard from "./layout/index";
-
 import Notify from "./components/Notify";
 import LoadingSpinner from "./components/LoadingSpinner";
+import DashboardTest from "./tests/Playground/DashboardTest";
 
 function App() {
   const status = useSelector((state) => state.root.status);
@@ -28,7 +27,14 @@ function App() {
           }
         />
         <Route path="login" element={<LoginPage />} />
-        <Route path="test" element={Dashboard()} />
+        {console.log(process.env.NODE_ENV)}
+        {
+          // playground is a dev feature, remove playground before offciail release
+          process.env.NODE_ENV === "development" ||
+          process.env.NODE_ENV === "test" ? (
+            <Route path="playground" element={DashboardTest()} />
+          ) : null
+        }
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <ToastContainer />
