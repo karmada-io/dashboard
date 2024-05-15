d2259570a194af3f1306c7448740fc1f950d690f
warjiang <1096409085@qq.com>
2024-05-15 11:53:30 +0800

    feat: add ui for propagationpolicy management
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ ui/apps/dashboard/src/pages/multicloud-policy-manage/propagation-policy/propagation-policy-editor-drawer.tsx
@@ -0,0 +1,108 @@
+import {FC, useEffect, useState} from 'react';
+import Editor from '@monaco-editor/react';
+import {Button, Drawer, Space} from "antd";
+import {editor} from "monaco-editor";
+import {CreatePropagationPolicy} from "@/services/propagationpolicy.ts";
+import {PutResource} from '@/services/unstructured'
+import {IResponse} from "@/services/base.ts";
+import {parse} from "yaml";
+import _ from 'lodash';
+export interface PropagationPolicyEditorDrawerProps {
+    open: boolean;
+    mode: 'create' | 'edit' | 'detail';
+    name?: string;
+    namespace?: string;
+    propagationContent?: string;
+    onClose: () => void
+    onUpdate: (ret: IResponse<string>) => void;
+    onCreate: (ret: IResponse<string>) => void;
+}
+
+function getTitle(mode: PropagationPolicyEditorDrawerProps['mode'], name: string = '') {
+    switch (mode) {
+        case "create":
+            return '新增调度策略';
+        case "edit":
+            return `编辑${name}调度策略`;
+        case "detail":
+            return `${name}策略详情`;
+        default:
+            return ''
+    }
+}
+
+const PropagationPolicyEditorDrawer: FC<PropagationPolicyEditorDrawerProps> = (props) => {
+    const {
+        open, mode, name,
+        namespace, propagationContent,
+        onClose, onCreate, onUpdate,
+    } = props
+    const [content, setContent] = useState<string>(propagationContent || '')
+    useEffect(() => {
+        setContent(propagationContent || '')
+    }, [propagationContent]);
+
+    function handleEditorChange(value: string | undefined, _: editor.IModelContentChangedEvent) {
+        setContent(value || '')
+    }
+
+    return <Drawer
+        open={open}
+        title={getTitle(mode, name)}
+        width={800}
+        styles={{
+            body: {padding: 0}
+        }}
+        onClose={onClose}
+        footer={
+            <div className={'flex flex-row justify-end'}>
+                <Space>
+                    <Button onClick={onClose}>取消</Button>
+                    <Button
+                        type="primary"
+                        onClick={async () => {
+                            const yamlObject = parse(content)
+                            if (mode === 'edit') {
+                                const updateRet = await PutResource({
+                                    kind:'propagationpolicy',
+                                    name: name || '',
+                                    namespace: namespace || '',
+                                    content: yamlObject
+                                })
+                                onUpdate(updateRet)
+                            } else {
+                                const name = _.get(yamlObject, 'metadata.name')
+                                const namespace = _.get(yamlObject, 'metadata.namespace')
+                                const createRet = await CreatePropagationPolicy({
+                                    isClusterScope: false,
+                                    name: name || '',
+                                    namespace: namespace || '',
+                                    propagationData: content,
+                                })
+                                onCreate(createRet)
+                            }
+                        }}>
+                        确定
+                    </Button>
+                </Space>
+            </div>
+        }
+    >
+        <Editor
+            defaultLanguage="yaml"
+            value={content}
+            theme='vs-dark'
+            // height={800}
+            options={{
+                theme: 'vs-dark',
+                lineNumbers: 'on',
+                readOnly: mode === 'detail',
+                minimap: {
+                    enabled: false,
+                },
+            }}
+            onChange={handleEditorChange}
+        />
+    </Drawer>
+}
+export default PropagationPolicyEditorDrawer;
