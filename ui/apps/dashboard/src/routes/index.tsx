955a8f8b0d2492300618241878d07841ac40dc18
warjiang <1096409085@qq.com>
2024-09-02 15:13:41 +0800

    feat: use pathPrefix in react-router and axios
    Signed-off-by: warjiang <1096409085@qq.com>

--- ui/apps/dashboard/src/routes/index.tsx
+++ ui/apps/dashboard/src/routes/index.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { routes } from './route.tsx';
+import { routerBase } from '@/services/base';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: routerBase,
+});
 export default function Router() {
   return <RouterProvider router={router} />;
 }
