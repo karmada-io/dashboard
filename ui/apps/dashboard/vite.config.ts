2d5712323a5a611c8652ad2ff7778c596a4528a3
warjiang <1096409085@qq.com>
2024-09-02 15:09:44 +0800

    feat: add vite-plugin-dynamic-base dependency & config dynamic path
    Signed-off-by: warjiang <1096409085@qq.com>

--- ui/apps/dashboard/vite.config.ts
+++ ui/apps/dashboard/vite.config.ts
@@ -2,13 +2,21 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import { dynamicBase } from 'vite-plugin-dynamic-base';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     base: process.env.NODE_ENV === 'development' ? '' : '/static',
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      dynamicBase({
+        publicPath: 'window.__dynamic_base__',
+        transformIndexHtml: true,
+      }),
+    ],
     resolve: {
       alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
