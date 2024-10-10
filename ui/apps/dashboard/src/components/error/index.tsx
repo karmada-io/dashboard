174b6d22de35ee27b8173979ed74a44e10c5a9c3
warjiang <1096409085@qq.com>
2024-10-10 17:50:30 +0800

    feat: add sentry monitor
    Signed-off-by: warjiang <1096409085@qq.com>

--- ui/apps/dashboard/src/components/error/index.tsx
+++ ui/apps/dashboard/src/components/error/index.tsx
@@ -1,8 +1,15 @@
 import i18nInstance from '@/utils/i18n';
 import type { ResultProps } from 'antd';
 import { Result } from 'antd';
+import { useRouteError } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { useEffect } from 'react';
 
 const ErrorBoundary = (props: ResultProps) => {
+  const error = useRouteError() as Error;
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
   return (
     <Result
       style={{ marginTop: '50vh', transform: 'translateY(-50%)' }}
--- ui/apps/dashboard/src/main.tsx
+++ ui/apps/dashboard/src/main.tsx
@@ -1,57 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import i18nInstance, {getLang} from '@/utils/i18n';
-import {initReactI18next} from 'react-i18next';
-import {loader} from '@monaco-editor/react';
+import i18nInstance, { getLang } from '@/utils/i18n';
+import { initReactI18next } from 'react-i18next';
+import { loader } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor';
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 // https://github.com/remcohaszing/monaco-yaml/issues/150
 import yamlWorker from '@/utils/workaround-yaml.worker?worker';
 import enTexts from '../locales/en-US.json';
 import zhTexts from '../locales/zh-CN.json';
-import {initRoute} from '@/routes/route.tsx';
+import { initRoute } from '@/routes/route.tsx';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://5edc679922b621e4ea52b1f66f24f48d@o4507968956268544.ingest.us.sentry.io/4507968984711168',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 window.MonacoEnvironment = {
-    getWorker(_, label) {
-        if (label === 'yaml') {
-            return new yamlWorker();
-        }
-        return new editorWorker();
-    },
+  getWorker(_, label) {
+    if (label === 'yaml') {
+      return new yamlWorker();
+    }
+    return new editorWorker();
+  },
 };
-loader.config({monaco});
+loader.config({ monaco });
 
 i18nInstance
-    .use(initReactI18next) // passes i18n down to react-i18next
-    .init({
-        debug: true,
-        lng: getLang(), // if you're using a language detector, do not define the lng option
-        fallbackLng: ['zh-CN'],
-        resources: {
-            zh: {
-                translation: zhTexts,
-            },
-            en: {
-                translation: enTexts,
-            },
-        },
-        interpolation: {
-            escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-        },
-        saveMissing: true, // send not translated keys to endpoint,
-        react: {
-            useSuspense: false,
-        },
-    })
-    .then(() => {
-        initRoute();
-        ReactDOM.createRoot(document.getElementById('root')!).render(
-            <React.StrictMode>
-                <App/>
-            </React.StrictMode>,
-        );
-    })
-    .catch(() => {
-
-    })
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    debug: true,
+    lng: getLang(), // if you're using a language detector, do not define the lng option
+    fallbackLng: ['zh-CN'],
+    resources: {
+      zh: {
+        translation: zhTexts,
+      },
+      en: {
+        translation: enTexts,
+      },
+    },
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+    saveMissing: true, // send not translated keys to endpoint,
+    react: {
+      useSuspense: false,
+    },
+  })
+  .then(() => {
+    initRoute();
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  })
+  .catch(() => {});
