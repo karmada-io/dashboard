6135bb1248f51067166a3ceb71e3d8510855342b
Heylosky <2547226479@qq.com>
2024-10-30 09:39:23 +0800

    add get member cluster node
    Signed-off-by: Heylosky <2547226479@qq.com>

--- pkg/resource/node/list.go
+++ pkg/resource/node/list.go
@@ -14,12 +14,10 @@ import (
 )
 
 type Node struct {
-	ObjectMeta         types.ObjectMeta       `json:"objectMeta"`
-	TypeMeta           types.TypeMeta         `json:"typeMeta"`
-	Ready              metav1.ConditionStatus `json:"ready,omitempty"`
-	KubernetesVersion  string                 `json:"kubernetesVersion,omitempty"`
-	NodeSummary        *v1alpha1.NodeSummary  `json:"nodeSummary,omitempty"`
-	AllocatedResources NodeAllocatedResources `json:"allocatedResources"`
+	ObjectMeta  types.ObjectMeta      `json:"objectMeta"`
+	TypeMeta    types.TypeMeta        `json:"typeMeta"`
+	NodeSummary *v1alpha1.NodeSummary `json:"nodeSummary,omitempty"`
+	Status      v1.NodeStatus         `json:"status"`
 }
 
 // NodeList contains a list of node.
@@ -57,10 +55,11 @@ func GetNodeListFromChannels(channels *common.ResourceChannels, dsQuery *datasel
 	return result, nil
 }
 
-func toNode(meta metav1.ObjectMeta) Node {
+func toNode(meta metav1.ObjectMeta, status v1.NodeStatus) Node {
 	return Node{
 		ObjectMeta: types.NewObjectMeta(meta),
 		TypeMeta:   types.NewTypeMeta(types.ResourceKindNode),
+		Status:     NewStatus(status),
 	}
 }
 
@@ -76,8 +75,19 @@ func toNodeList(nodes []v1.Node, nonCriticalErrors []error, dsQuery *dataselect.
 	result.ListMeta = types.ListMeta{TotalItems: filteredTotal}
 
 	for _, item := range nodes {
-		result.Items = append(result.Items, toNode(item.ObjectMeta))
+		result.Items = append(result.Items, toNode(item.ObjectMeta, item.Status))
 	}
 
 	return result
+}
+
+func NewStatus(status v1.NodeStatus) v1.NodeStatus {
+	return v1.NodeStatus{
+		Capacity:        status.Capacity,
+		Allocatable:     status.Allocatable,
+		Conditions:      status.Conditions,
+		Addresses:       status.Addresses,
+		DaemonEndpoints: status.DaemonEndpoints,
+		NodeInfo:        status.NodeInfo,
+	}
 }
