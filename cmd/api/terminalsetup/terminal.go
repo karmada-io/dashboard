b3750aeb8fd0096eb3327e773c593fb76ebdc1ea
MdSayemkhan <sayemkhan4604@gmail.com>
2025-05-26 00:49:10 +0600

    Fix build and lint errors after PR review
    Signed-off-by: MdSayemkhan <sayemkhan4604@gmail.com>

--- cmd/api/terminalsetup/terminal.go
+++ cmd/api/terminalsetup/terminal.go
@@ -40,7 +40,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/client-go/tools/remotecommand"
-	import "k8s.io/utils/ptr" 
+	"k8s.io/utils/ptr" 
 )
 
 // waitForPodReady polls until the Pod is Running and Ready, or times out.
