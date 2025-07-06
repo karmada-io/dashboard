b684af968d3ec440c3a6d8ffc509bf367c0b3435
warjiang <1096409085@qq.com>
2025-07-06 22:12:26 +0800

    feat(api): add terminal backend api
    Co-authored-by: MdSayemkhan <sayemkhan4604@gmail.com>
    Signed-off-by: warjiang <1096409085@qq.com>

--- pkg/resource/common/resourcestatus.go
+++ pkg/resource/common/resourcestatus.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//revive:disable:var-naming
 package common
 
 // ResourceStatus provides basic information about resources status on the list.
