e82e98bf7d8af970b9e810b429a32f1174f8238e
warjiang <1096409085@qq.com>
2025-07-06 22:12:26 +0800

    feat(api): add terminal backend api
    Co-authored-by: MdSayemkhan <sayemkhan4604@gmail.com>
    Signed-off-by: warjiang <1096409085@qq.com>

--- cmd/api/app/types/common/response.go
+++ cmd/api/app/types/common/response.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//revive:disable:var-naming
 package common
 
 import (
--- pkg/common/types/resourcekind.go
+++ pkg/common/types/resourcekind.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//revive:disable:var-naming
 package types
 
 // ResourceKind is an unique name for each resource. It can used for API discovery and generic
