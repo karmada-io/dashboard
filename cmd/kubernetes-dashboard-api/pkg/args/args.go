b0b4a8a6b63036bc70c55157b3134d53a9dd6662
warjiang <1096409085@qq.com>
2025-07-08 00:47:50 +0800

    feat(kubernetes-dashboard-api): update initialization of kubernetes dashboard api Co-authored-by: MdSayemkhan <sayemkhan4604@gmail.com>
    Signed-off-by: warjiang <1096409085@qq.com>

--- cmd/kubernetes-dashboard-api/pkg/args/args.go
+++ cmd/kubernetes-dashboard-api/pkg/args/args.go
@@ -74,6 +74,10 @@ var (
 	argKubeConfigFile            = pflag.String("kubeconfig", "", "path to kubeconfig file with control plane location information")
 	argNamespace                 = pflag.String("namespace", helpers.GetEnv("POD_NAMESPACE", "kubernetes-dashboard"), "Namespace to use when accessing Dashboard specific resources, i.e. metrics scraper service")
 	argMetricsScraperServiceName = pflag.String("metrics-scraper-service-name", "kubernetes-dashboard-metrics-scraper", "name of the dashboard metrics scraper service")
+
+	argKarmadaKubeConfigFile         = pflag.String("karmada-kubeconfig", "", "path to karmada kubeconfig file with karmada control plane location information")
+	argKarmadaContext                = pflag.String("karmada-context", "", "name of the karmada-kubeconfig context to use")
+	argKarmadaApiserverSkipTLSVerify = pflag.Bool("karmada-apiserver-skip-tls-verify", false, "enable if connection with remote Karmada API server should skip TLS verify")
 )
 
 func init() {
@@ -181,4 +185,16 @@ func IsProxyEnabled() bool {
 
 func IsOpenAPIEnabled() bool {
 	return *argOpenAPIEnabled
+}
+
+func KarmadaKubeConfigPath() string {
+	return *argKarmadaKubeConfigFile
+}
+
+func KarmadaContext() string {
+	return *argKarmadaContext
+}
+
+func KarmadaApiserverSkipTLSVerify() bool {
+	return *argKarmadaApiserverSkipTLSVerify
 }
