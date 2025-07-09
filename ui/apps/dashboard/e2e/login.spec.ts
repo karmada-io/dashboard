b467bfc3cb2d23849fcaabbef048902467b0b470
warjiang <1096409085@qq.com>
2025-07-09 13:38:58 +0800

    test(e2e): add example test for login page
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ ui/apps/dashboard/e2e/login.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('should navigate to the login page', async ({ page }) => {
+  // Navigate to the home page
+  await page.goto('/');
+
+  await page.waitForTimeout(2 * 1000);
+
+  await expect(page.getByText('Sign in')).toBeVisible({
+    timeout: 10000,
+  });
+});
+
+/*
+test('should navigate to overview after login', async ({ page }) => {
+  // Navigate to the home page
+  await page.goto('/');
+
+  await page.waitForTimeout(2 * 1000);
+
+  await page.getByTestId('login-input').fill('{token placeholder}')
+
+  await page.getByTestId('login-button').click()
+  await expect(page).toHaveURL(/overview/, {
+    timeout: 5* 1000
+  })
+});
+*/
---
+++ ui/apps/dashboard/playwright.config.ts
@@ -0,0 +1,61 @@
+import { defineConfig, devices } from '@playwright/test';
+
+// Use process.env.PORT by default and fallback to port 3000
+const PORT = process.env.PORT || 5173;
+
+// Set webServer.url and use.baseURL with the location of the WebServer
+const baseURL = `http://localhost:${PORT}`;
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  // Timeout per test
+  timeout: 30 * 1000,
+
+  // Look for test files in the "tests" directory, relative to this configuration file.
+  testDir: 'e2e',
+
+  // Retry on CI only.
+  retries: process.env.CI ? 2 : 0,
+
+  // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
+
+  // Reporter to use
+  reporter: 'html',
+
+  // Artifacts folder where screenshots, videos, and traces are stored.
+  outputDir: 'test-results/',
+
+  use: {
+    // Use baseURL so to make navigations relative.
+    // More information: https://playwright.dev/docs/api/class-page#page-goto
+    baseURL,
+
+    // Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs, network traffic etc.
+    // More information: https://playwright.dev/docs/trace-viewer
+    trace: 'retry-with-trace',
+
+    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
+    // contextOptions: {
+    //   ignoreHTTPSErrors: true,
+    // },
+  },
+
+  // Configure projects for major browsers.
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  // Run your local dev server before starting the tests:
+  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+  webServer: {
+    command: 'pnpm dev',
+    url: baseURL,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+});
