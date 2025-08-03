1569bdbdd9b19b7ce2751bc1c98d6ab928b2d2c9
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2025-08-03 22:38:36 +0800

    fix: recreate e2e files with correct minimal content

--- ui/apps/dashboard/playwright.config.ts
+++ ui/apps/dashboard/playwright.config.ts
@@ -16,59 +16,26 @@ limitations under the License.
 
 import { defineConfig, devices } from '@playwright/test';
 
-// Use process.env.PORT by default and fallback to port 5173
 const PORT = process.env.PORT || 5173;
-
-// Set webServer.url and use.baseURL with the location of the WebServer
 const baseURL = `http://localhost:${PORT}`;
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  // Timeout per test
   timeout: 30 * 1000,
-
-  // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: 'e2e',
-
-  // Retry on CI only.
   retries: process.env.CI ? 2 : 0,
-
-  // Opt out of parallel tests on CI.
   workers: process.env.CI ? 1 : undefined,
-
-  // Reporter to use
   reporter: 'html',
-
-  // Artifacts folder where screenshots, videos, and traces are stored.
   outputDir: 'test-results/',
-
   use: {
-    // Use baseURL so to make navigations relative.
-    // More information: https://playwright.dev/docs/api/class-page#page-goto
     baseURL,
-
-    // Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs,
-    network traffic etc.
-    // More information: https://playwright.dev/docs/trace-viewer
     trace: 'retry-with-trace',
-
-    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
-    // contextOptions: {
-    //   ignoreHTTPSErrors: true,
-    // },
   },
-
-  // Configure projects for major browsers.
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
     command: 'pnpm dev',
     url: baseURL,
