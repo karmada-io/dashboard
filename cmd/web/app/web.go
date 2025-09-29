warjiang/karmada-dashboard-v2

fix(karmada-dashboard): add auth check in middleware
Signed-off-by: warjiang <1096409085@qq.com>

cmd/web/app/web.go
@@ -97,6 +97,11 @@ func generateAPIProxy(remoteURL string, director func(*http.Request, *gin.Contex
 	}
 
 	return func(c *gin.Context) {
+		if c.Request.Header.Get("Authorization") == "" {
+			c.String(http.StatusForbidden, "Forbidden")
+			c.Abort()
+			return
+		}
 		proxy := httputil.NewSingleHostReverseProxy(remoteEndpoint)
 		originalDirector := proxy.Director
 		proxy.Director = func(req *http.Request) {

karmada-io/dashboard

cmd/web/app/web.go
@@ -97,6 +97,11 @@ func generateAPIProxy(remoteURL string, director func(*http.Request, *gin.Contex
 	}
 
 	return func(c *gin.Context) {
+		if c.Request.Header.Get("Authorization") == "" {
+			c.String(http.StatusUnauthorized, "Forbidden")
+			c.Abort()
+			return
+		}
 		proxy := httputil.NewSingleHostReverseProxy(remoteEndpoint)
 		originalDirector := proxy.Director
 		proxy.Director = func(req *http.Request) {